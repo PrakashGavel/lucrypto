8864eba93eaa396916d033ab228ef62aade39a91
PrakashGavel <prakashgavel1000@gmail.com>
2023-08-02 23:54:50 +0530

    a
    Commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBx9sYrXYDd61e45j8MARRsgwc89ggTmHM",
+    authDomain: "lu-crypto.firebaseapp.com",
+    projectId: "lu-crypto",
+    storageBucket: "lu-crypto.appspot.com",
+    messagingSenderId: "143056240874",
+    appId: "1:143056240874:web:a5a2734ec1bf008bdf1793"
+  };
+
+  export default firebaseConfig;
